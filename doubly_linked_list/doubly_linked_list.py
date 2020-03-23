b1c9cf50eb9ac2d9fb5638d2d0d15974e33b9880
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-23 15:29:02 -0400

    finished move to front and back and max functions

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -103,13 +103,17 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -128,4 +132,10 @@ class DoublyLinkedList:
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if max_value < current.value:
+                max_value = current.value
+            current = current.next
+        return max_value
