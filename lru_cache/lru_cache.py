89ac730261170df45bff010e0db93495bddbc9b1
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-24 16:30:48 -0400

    completed lru cache and all tests pass

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.storage = DoublyLinkedList() 
+        self.cache = {} # key : node pointer
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,15 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.cache:
+            node = self.cache[key]
+            self.storage.delete(node)
+            self.storage.add_to_head([key, node.value[1]])
+            return node.value[1]
+        else:
+            print(self.cache)
+            return None
+
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +43,17 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.cache:
+            node = self.cache[key]
+            self.storage.delete(node)
+            self.storage.add_to_head([key, value])
+            self.cache[key] = self.storage.head
+        elif len(self.cache) >= self.limit:
+            self.cache.pop(self.storage.tail.value[0])
+            self.storage.remove_from_tail()
+            self.storage.add_to_head([key,value])
+            self.cache[key] = self.storage.head
+        else:
+            self.size += 1
+            self.storage.add_to_head([key,value])
+            self.cache[key] = self.storage.head
